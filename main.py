8ad7dcf81e4a62016572a1b8a52577fc53687684
Axel Maldonado <axel@axelmaldonado.com>
2018-10-25 12:16:10 -0500

    Implemented caesar.py

---
+++ main.py
@@ -0,0 +1,54 @@
+from flask import Flask, request
+from caesar import rotate_string
+
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+form = """
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <style>
+            form {
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }
+            textarea {
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }
+        </style>
+    </head>
+    <body>
+        <form action="/" method="POST">
+            Rotate by: 
+            <input type="text" id="rot" name="rot" value="0"/>
+                <textarea id="textarea" name="textarea">
+            </textarea>
+        <input type="submit" value="Submit"/>
+    </form>
+    </body>
+</html>
+"""
+
+@app.route("/")
+def index():
+    return form
+
+@app.route("/", methods=['POST'])
+def encrypt():
+    web_rot = int(request.form['rot'])
+    web_text = request.form['textarea']
+
+    return "<h1>" + rotate_string(web_text, web_rot) +  "</h1>"
+
+
+
+app.run()
